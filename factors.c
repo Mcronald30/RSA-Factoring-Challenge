64059bf7d66a65cc21921a203eafb3da9ce5f5b3
Ronald <nottinsonstephen@gmail.com>
2023-05-26 10:29:55 -0700

    factors

---
+++ factors.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef long long int ll;
+
+void factorize(ll n)
+{
+	for (ll i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			printf("%lld=%lld*%lld\n", n, i, n / i);
+			n /= i;
+		}
+	}
+	if (n > 1)
+	{
+		printf("%lld=%lld*%lld\n", n, n, 1LL);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Usage: factors <file>\n");
+		return (1);
+	}
+
+	FILE *file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		printf("Error opening file.\n");
+		return (1);
+	}
+
+	ll number;
+	while (fscanf(file, "%lld", &number) == 1)
+	{
+		factorize(number);
+	}
+
+	fclose(file);
+	return (0);
+}
